3c4e46bb122c0d7b6a808b5db87144623e3c628e
Kunaal Parekh <kunparekh18v1@gmail.com>
2020-02-02 19:45:50 +0530

    Frontend additions for competitors

--- client/src/app/pages/competitors/competitors.component.ts
+++ client/src/app/pages/competitors/competitors.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-competitors',
@@ -7,9 +9,103 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompetitorsComponent implements OnInit {
 
-  constructor() { }
+//public event_flags={"2":false, "3":false, "4":false, "5":false, "6":false, "7":false, "oh":false, "fmc":false, "3b":false, "4b":false, "5b":false, "mb":false, "sk":false, "py":false, "me":false, "cl":false, "sq":false };
+
+  public eventIconUrls = {
+
+    'three' : "https://i.ibb.co/WxZD8Xq/1-3x3x3.png",
+    'two' : "https://i.ibb.co/M8S3fWB/2-2x2x2.png",
+    'four' : "https://i.ibb.co/pj1wY2Z/3-4x4x4.png",
+    'five' : "https://i.ibb.co/P6RxKQs/4-5x5x5.png",
+    'six' : "https://i.ibb.co/k80pHf0/5-6x6x6.png",
+    'seven' : "https://i.ibb.co/27jmpP0/6-7x7x7.png",
+    'bld3' : "https://i.ibb.co/WPtpmn6/7-3x3x3-Blindfolded.png",
+    'fmc' : "https://i.ibb.co/HntJjH7/8-3x3x3-Fewest-Moves.png",
+    'oh' : "https://i.ibb.co/jTdn0MJ/9-3x3x3-One-Handed.png",
+    'clock' : "https://i.ibb.co/prmXRVY/11-Clock.png",
+    'mega' : "https://i.ibb.co/0fkNWmm/12-Megaminx.png",
+    'pyra' : "https://i.ibb.co/pW5V53T/13-Pyraminx.png",
+    'skewb' : "https://i.ibb.co/60tqYK8/14-Skewb.png",
+    'sq1' : "https://i.ibb.co/xznhKX5/15-Square-1.png",
+    'bld4' : "https://i.ibb.co/6JHQzXX/16-4x4x4-Blindfolded.png",
+    'bld5' : "https://i.ibb.co/7WZsGCk/17-5x5x5-BLindfolded.png",
+    'mbld' : "https://i.ibb.co/P5cyF5G/18-3x3x3-Multi-Blind.png",
+
+
+  }
+
+  competitorsDataSource: CompetitorRegistrationData[] = [];
+
+  constructor(private httpClient: HttpClient) {
+    
+    this.httpClient.get(environment.baseApiUrl + "/api/payment/getCompetitors").subscribe((res: {competitors}) => {
+      
+      res.competitors.array.forEach(element => {
+        this.competitorsDataSource.push( <CompetitorRegistrationData> {
+          name: element.name,
+          citizenOf: element.country,
+          total: element.events.length,
+
+          three: (element.events.includes('3') ? this.eventIconUrls.three : ''),
+          two: (element.events.includes('2') ? this.eventIconUrls.two : ''),
+          four: (element.events.includes('4') ? this.eventIconUrls.four : ''),
+          five: (element.events.includes('5') ? this.eventIconUrls.five : ''),
+
+          six: (element.events.includes('6') ? this.eventIconUrls.six : ''),
+          seven: (element.events.includes('7') ? this.eventIconUrls.seven : ''),
+          bld3: (element.events.includes('3b') ? this.eventIconUrls.bld3 : ''),
+          fmc: (element.events.includes('fmc') ? this.eventIconUrls.fmc : ''),
+
+          oh: (element.events.includes('oh') ? this.eventIconUrls.oh : ''),
+          clock: (element.events.includes('cl') ? this.eventIconUrls.clock : ''),
+          mega: (element.events.includes('me') ? this.eventIconUrls.mega : ''),
+          pyra: (element.events.includes('py') ? this.eventIconUrls.pyra : ''),
+
+          skewb: (element.events.includes('sk') ? this.eventIconUrls.skewb : ''),
+          sq1: (element.events.includes('sq') ? this.eventIconUrls.sq1 : ''),
+          bld4: (element.events.includes('4b') ? this.eventIconUrls.bld4 : ''),
+          bld5: (element.events.includes('5b') ? this.eventIconUrls.bld5 : ''),
+          mbld: (element.events.includes('mb') ? this.eventIconUrls.mbld : ''),
+
+        })  
+      });
+
+    });
+
+   }
 
   ngOnInit() {
   }
 
+  
+
 }
+
+export interface CompetitorRegistrationData {
+  
+  name: string;
+  citizenOf: string;
+
+  //events
+  three: string;
+  two: string;
+  four: string;
+  five: string;
+  six: string;
+  seven: string;
+  bld3: string;
+  fmc: string;
+  oh: string;
+  clock: string;
+  mega: string;
+  pyra: string;
+  skewb: string;
+  sq1: string;
+  bld4: string;
+  bld5: string;
+  mbld: string;
+
+
+  total:string;
+}
+
