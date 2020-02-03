0af0d95255717cda516f4b871b7a0f54a2dee345
Daniel James <daniel.james@zohocorp.com>
2020-02-03 13:04:27 +0530

    fix try

--- server/routes/payment/createOrder.js
+++ server/routes/payment/createOrder.js
@@ -63,7 +63,8 @@ function getBaseFee() {
 
 async function createOrder(req, res, db, events, order) {
     console.log(order)
-    console.log(order["payment_request"])
+    order = JSON.parse(order)
+    console.log(order.payment_request)
     await db.registrations.findOneAndUpdate(
         { _id: req.session.userId },
         {$set: {
@@ -71,7 +72,7 @@ async function createOrder(req, res, db, events, order) {
                 events: events,
                 regOrderId: order
             },
-            lastPaymentId: order["payment_request"].id
+            lastPaymentId: order.payment_request.id
         }},
         { upsert: true, returnOriginal: false }
     )
