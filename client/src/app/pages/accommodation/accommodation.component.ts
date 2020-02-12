e3127e39237ba8d5450a525b48a25809ec40a5e6
Saravanan Gowthaman <sarav200898@gmail.com>
2020-02-12 20:29:43 +0530

    acccomodation bug fix + changes

--- client/src/app/pages/accommodation/accommodation.component.ts
+++ client/src/app/pages/accommodation/accommodation.component.ts
@@ -19,7 +19,10 @@ export class AccommodationComponent implements OnInit {
   fullcout=moment();
   fulltype : number;
   amount=0;
-  accomodationChoice="t1";
+  //accomodationChoice="t1";
+  sharein =0;
+  shareout =0;
+
   prefer: String;
 
   addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
@@ -40,10 +43,16 @@ export class AccommodationComponent implements OnInit {
 
 payshare() {
   console.log();
+  // if(this.selected=='shared2')
+  // (this.accomodationChoice=="t1")?this.amount=2700:(this.accomodationChoice=="t2")?this.amount=3600:  (this.accomodationChoice=="t3")?this.amount=3600:(this.accomodationChoice=="t4")?this.amount=4500:this.amount=0;
+  // if(this.selected=='shared3')
+  // (this.accomodationChoice=="t1")?this.amount=2400:(this.accomodationChoice=="t2")?this.amount=3200:  (this.accomodationChoice=="t3")?this.amount=3200:(this.accomodationChoice=="t4")?this.amount=4000:this.amount=0;
   if(this.selected=='shared2')
-  (this.accomodationChoice=="t1")?this.amount=2700:(this.accomodationChoice=="t2")?this.amount=3600:  (this.accomodationChoice=="t3")?this.amount=3600:(this.accomodationChoice=="t4")?this.amount=4500:this.amount=0;
+  this.amount=Math.max(0, ((this.shareout - this.sharein +1)*900 ))
+  
   if(this.selected=='shared3')
-  (this.accomodationChoice=="t1")?this.amount=2400:(this.accomodationChoice=="t2")?this.amount=3200:  (this.accomodationChoice=="t3")?this.amount=3200:(this.accomodationChoice=="t4")?this.amount=4000:this.amount=0;
+  this.amount=Math.max(0, ((this.shareout - this.sharein +1)*800 ))
+
   if(this.mainselect=='main2'){
     this.amount=Math.max(0, ((this.dormout - this.dormin +1)*250 ))
   }
