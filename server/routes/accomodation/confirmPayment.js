ea68b2ece68773b529189334505be918ba746514
Kunaal Parekh <kunparekh18v1@gmail.com>
2020-02-14 00:40:35 +0530

    Comment out DB code in accomodation/confirmPayment route

--- server/routes/accomodation/confirmPayment.js
+++ server/routes/accomodation/confirmPayment.js
@@ -48,26 +48,26 @@ module.exports = function(req, res, db) {
 }
 
 async function updateFailedReg(req, res, db) {
-    await db.registrations.findOneAndUpdate(
-        { lastPaymentId: req.body.payment_request_id },
-        {$set: {
-            failedStatus: req.body
-        }},
-        { upsert: true, returnOriginal: false }
-    )
+    // await db.registrations.findOneAndUpdate(
+    //     { lastPaymentId: req.body.payment_request_id },
+    //     {$set: {
+    //         failedStatus: req.body
+    //     }},
+    //     { upsert: true, returnOriginal: false }
+    // )
     res.send({
         success: false
     })
 }
 
 async function updateReg(req, res, db) {
-    await db.registrations.findOneAndUpdate(
-        { lastPaymentId: req.body.payment_request_id },
-        {$set: {
-            regPaid: true
-        }},
-        { upsert: true, returnOriginal: false }
-    )
+    // await db.registrations.findOneAndUpdate(
+    //     { lastPaymentId: req.body.payment_request_id },
+    //     {$set: {
+    //         regPaid: true
+    //     }},
+    //     { upsert: true, returnOriginal: false }
+    // )
     res.send({
         success: true
     })
