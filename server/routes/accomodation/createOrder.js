b6daa6a0490d5ca9ea2df209bfa44d5429080c92
Kunaal Parekh <kunparekh18v1@gmail.com>
2020-02-15 00:18:00 +0530

    Fix incorrect request to Instamojo in accomodation/createPayment route

--- server/routes/accomodation/createOrder.js
+++ server/routes/accomodation/createOrder.js
@@ -5,19 +5,18 @@ const {INSTAMOJO_API_KEY, INSTAMOJO_AUTH_KEY} = require('../../config');
 
 module.exports = function(req, res, db) {
     if (req.query.accomodationData) {
+        accomodationData = JSON.parse(req.query.accomodationData);
         
         Insta.setKeys(INSTAMOJO_API_KEY,INSTAMOJO_AUTH_KEY);
         // Insta.isSandboxMode(true); //Used to test with test.instamojo.com. Remove in production
 
         var instaPaymentData = new Insta.PaymentData();
         instaPaymentData.purpose = 'Nats20 Accomodation Registration';
-        instaPaymentData.amount = req.query.accomodationData.amount;
+        instaPaymentData.amount = accomodationData.amount;
         instaPaymentData.webhook = req.query.webhook;
         instaPaymentData.currency = 'INR';
 
-
         if (req.session.isLoggedIn == true) {
-
             Insta.createPayment(instaPaymentData, function(error, response) {
                 if (error) {
                     res.status(error.statusCode).send(error);
